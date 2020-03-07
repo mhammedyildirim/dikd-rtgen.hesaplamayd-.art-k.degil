d66a373f47ac24b6a88772ae2dcfcb95522142b7
mhammedyildirim <61433710+mhammedyildirim@users.noreply.github.com>
2020-03-07 23:24:56 +0300

    Initial commit

---
+++ dikdörtgen.hesaplama/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dikdörtgen.hesaplama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string isim;
+            int yas;
+            double boy;
+            
+            Console.Write("Lutfen isminizi giriniz:");
+             isim = Console.ReadLine();
+            //isimi aldık kullanıcıdan
+            Console.Write("Lutfen yasinizi giriniz:");
+            yas = Convert.ToInt32(Console.ReadLine());
+            //yasi aldik simdi de
+            Console.Write("Lutfen boyunuzu cm cinsinden giriniz:");
+            boy = Convert.ToInt32(Console.ReadLine());
+            //simdi de boyu aldık
+
+            Console.WriteLine("Merhaba {0} bey {1} yasindasiniz {2} boyundasiniz", isim, yas, boy);
+
+            Console.ReadKey();
+
+        }
+    }
+}
